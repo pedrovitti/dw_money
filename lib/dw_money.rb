868368c61945141b1089cf9262b91c43c502eab7
Pedro Vitti <pedrovitti@gmail.com>
2017-06-01 23:36:09 +0200

    Generalize math operator methods.

--- lib/dw_money.rb
+++ lib/dw_money.rb
@@ -36,36 +36,13 @@ module DwMoney
       amount.round(2) <=> other.convert_to(currency).amount.round(2)
     end
 
-    def +(other)
-      raise TypeError unless (other.is_a?(Money) || other.is_a?(Numeric))
+    [:+, :-,:*, :/].each do |math_operator|
+      define_method(math_operator) do |other|
+        raise TypeError unless (other.is_a?(Money) || other.is_a?(Numeric))
 
-      other = other.convert_to(currency).amount if other.is_a?(Money)
-
-      Money.new(amount + other, currency)
-    end
-
-    def -(other)
-      raise TypeError unless (other.is_a?(Money) || other.is_a?(Numeric))
-
-      other = other.convert_to(currency).amount if other.is_a?(Money)
-
-      Money.new(amount - other, currency)
-    end
-
-    def *(other)
-      raise TypeError unless (other.is_a?(Money) || other.is_a?(Numeric))
-
-      other = other.convert_to(currency).amount if other.is_a?(Money)
-
-      Money.new(amount * other, currency)
-    end
-
-    def /(other)
-      raise TypeError unless (other.is_a?(Money) || other.is_a?(Numeric))
-
-      other = other.convert_to(currency).amount if other.is_a?(Money)
-
-      Money.new(amount / other, currency)
+        other = other.convert_to(currency).amount if other.is_a?(Money)
+        Money.new(amount.public_send(math_operator, other), currency)
+      end
     end
 
     private
