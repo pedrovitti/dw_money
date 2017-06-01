5ee3d450d201c65827bb97901bf527ab4ee0b1f8
Pedro Vitti <pedrovitti@gmail.com>
2017-06-02 00:01:07 +0200

    Extract arithmetic logic to a module

---
+++ lib/dw_money/money.rb
@@ -0,0 +1,42 @@
+require "dw_money/money/arithmetic"
+
+module DwMoney
+  class ConversionRateNotFound < StandardError; end
+
+  class Money
+    include Comparable
+    include Money::Arithmetic
+
+    attr_reader :amount, :currency
+
+    @@conversion_rates = {}
+
+    def self.conversion_rates(base_currency, rates)
+      @@conversion_rates[base_currency] = rates
+    end
+
+    def initialize(amount, currency)
+      @amount = amount
+      @currency = currency
+    end
+
+    def inspect
+      "#{'%.2f' % amount} #{currency}"
+    end
+
+    def convert_to(new_currency)
+      return self if new_currency == currency
+
+      raise ConversionRateNotFound, "#{new_currency} => #{currency}" unless rates && rates[new_currency]
+
+      new_amount = amount * rates[new_currency]
+      Money.new(new_amount, new_currency)
+    end
+
+    private
+
+    def rates
+      @@conversion_rates[currency]
+    end
+  end
+end
--- spec/money/arithmetic_spec.rb
+++ spec/money/arithmetic_spec.rb
@@ -2,52 +2,6 @@ require "spec_helper"
 
 describe DwMoney::Money do
 
-  describe ".conversion_rates" do
-
-    it "configures rates for base currency" do
-      expected_rates = {
-        "BRL" => { 'EUR' => 0.28, 'USD' => 0.31 },
-        'EUR' => { 'USD' => 1.12, 'BRL' => 3.51 }
-      }
-
-      DwMoney::Money.conversion_rates("BRL", expected_rates["BRL"])
-      DwMoney::Money.conversion_rates("EUR", expected_rates["EUR"])
-
-      expect(DwMoney::Money.class_variable_get :@@conversion_rates).to include(expected_rates)
-    end
-
-  end
-
-  describe "#inspect" do
-    subject { DwMoney::Money.new(50.12, "EUR").inspect }
-
-    it "returns a readable version of amount and currency" do
-      expect(subject).to eq "50.12 EUR"
-    end
-  end
-
-  describe "#convert_to" do
-    before do
-      DwMoney::Money.conversion_rates("EUR", { 'USD' => 1.12, 'BRL' => 3.51 })
-    end
-
-    context "when converting money" do
-      let(:fifty_euros) { DwMoney::Money.new(50.12, "EUR") }
-
-      context "to an existing currency" do
-        it "returns money with converted amount and currency" do
-          expect(fifty_euros.convert_to("USD")).to eq DwMoney::Money.new(56.13, "USD")
-        end
-      end
-
-      context "to a non-existing currency" do
-        it "raises and error" do
-          expect { fifty_euros.convert_to("XUN") }.to raise_error(DwMoney::ConversionRateNotFound, "XUN => EUR")
-        end
-      end
-    end
-  end
-
   describe "#+" do
 
     context "when Money or Numeric objects" do
@@ -285,5 +239,4 @@ describe DwMoney::Money do
       end
     end
   end
-
 end
